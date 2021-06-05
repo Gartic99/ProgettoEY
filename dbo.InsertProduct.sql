c9ee69ff933d3aea13dc1f665633e6f6e545f9e8
Gartic99 <artic.1804049@studenti.uniroma1.it>
2021-06-05 20:32:57 +0200

    aggiunta sotored procedure

---
+++ dbo.InsertProduct.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[InsertProduct]
+	@ProductCategoryID int,
+	@ProductModelID int,
+	@Name NVarChar(50),
+	@ProductNumber NVarChar(25),
+	@StandardCost money,
+	@ListPrice money,
+	@SellStartDate datetime,
+	@ModifiedDate datetime
+
+AS
+	PRINT @Name; 
+	INSERT INTO SalesLT.Product
+	(ProductCategoryID, ProductModelID,Name,ProductNumber,StandardCost,ListPrice,SellStartDate,Rowguid,ModifiedDate)
+	VALUES
+	(@ProductCategoryID,@ProductModelID,@Name,@ProductNumber,@StandardCost,
+	@ListPrice,
+	@SellStartDate,
+	NEWID() ,
+	@ModifiedDate);
